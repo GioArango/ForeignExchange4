09541e5dce18e4fba3dd6c22e390ff88977a08d1
GioArango <gioarango.94@gmail.com>
2017-08-20 14:48:06 -0500

    Create Model, propierties, Bindings, video 3

---
+++ ForeignExchange4/ForeignExchange4/ViewModels/MainViewModel.cs
@@ -0,0 +1,82 @@
+﻿namespace ForeignExchange4.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using Models;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using System;
+
+    public class MainViewModel
+    {
+        #region Propierties
+
+        public string Amount
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<Rate> Rates
+        {
+            get;
+            set;
+        }
+
+        public Rate SourceRate
+        {
+            get;
+            set;
+        }
+
+        public Rate TargetRate
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsEnabled
+        {
+            get;
+            set;
+        }
+
+        public string Result
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand ConvertCommand
+        {
+            get
+            {
+                return new RelayCommand(Convert);
+            }
+            
+        }
+
+        void Convert()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
