6975d807d022938d92e441f58f4cf4ed38654e05
GioArango <gioarango.94@gmail.com>
2017-08-27 22:59:05 -0500

    Video 13

---
+++ ForeignExchange4/ForeignExchange4.Android/Implementations/Config.cs
@@ -0,0 +1,42 @@
+﻿using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ForeignExchange4.Droid.Implementations.Config))]
+
+namespace ForeignExchange4.Droid.Implementations
+{
+    using Interfaces;
+    using SQLite.Net.Interop;
+
+    public class Config : IConfig
+    {
+        private string directoryDB;
+        private ISQLitePlatform platform;
+
+        public string DirectoryDB
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(directoryDB))
+                {
+                    directoryDB = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+                }
+
+                return directoryDB;
+            }
+        }
+
+        public ISQLitePlatform Platform
+        {
+            get
+            {
+                if (platform == null)
+                {
+                    platform = new SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid();
+                }
+
+                return platform;
+
+            }
+        }
+    }
+}
---
+++ ForeignExchange4/ForeignExchange4.iOS/Implementations/Config.cs
@@ -0,0 +1,44 @@
+﻿using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ForeignExchange4.iOS.Implementations.Config))]
+
+namespace ForeignExchange4.iOS.Implementations
+{
+    using Interfaces;
+    using SQLite.Net.Interop;
+    using System;
+
+    public class Config : IConfig
+    {
+        private string directoryDB;
+        private ISQLitePlatform platform;
+
+        public string DirectoryDB
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(directoryDB))
+                {
+                    var directory = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                    directoryDB = System.IO.Path.Combine(directory, "..", "Library");
+                }
+
+                return directoryDB;
+            }
+        }
+
+        public ISQLitePlatform Platform
+        {
+            get
+            {
+                if (platform == null)
+                {
+                    platform = new SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS();
+                }
+
+                return platform;
+            }
+        }
+    }
+
+}
